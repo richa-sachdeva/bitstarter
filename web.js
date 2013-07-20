efe074aa9496cd24a7758f54e8f646cce67d1a14
Ubuntu <ubuntu@ip-172-31-17-216.us-west-2.compute.internal>
2013-07-20 03:52:52 +0000

    updated web.js #1

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.writeFile('index.html', function(err){ 
+	if(err) throw err;
+	console.log('It\'s saved!');
+	});
 });
 
 var port = process.env.PORT || 5000;
